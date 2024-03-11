d247d424ecc79b6b355812c31df7ba9d3444dff5
Daaski <Redkin.Artem.2017@yandex.ru>
2024-03-11 22:16:08 +0300

    initial commit

--- src/utils/calculateDoggiFood.ts
+++ src/utils/calculateDoggiFood.ts
@@ -14,8 +14,8 @@ export function calculateDoggiFood(
         for (let j = 0; j < amountTimesPerDay; j++) {
             newFoodPercent += 100 / totalAmountOperations;
             dayArray.push({
-                newFood: amountFoodPerTime * (newFoodPercent / 100),
-                oldFood: amountFoodPerTime * ((100 - newFoodPercent) / 100),
+                newFood: +(amountFoodPerTime * (newFoodPercent / 100)).toFixed(1),
+                oldFood: +(amountFoodPerTime * ((100 - newFoodPercent) / 100)).toFixed(1),
             });
             arrayFood.set(`День: ${i}`, dayArray);
         }
